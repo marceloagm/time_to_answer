440cb66ebded605630a839c93e2125ae7e5bf108
marceloagm <marceloagmagalhaes@gmail.com>
2020-02-12 19:38:50 +0000

    Configurando Layout Site

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,8 +1,7 @@
 class Site::SearchController < SiteController
 
     def questions
-    
-    @questions = Question.all
-
+       
+    @questions = Question._search_(params[:page], params[:term])
     end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,14 @@ class Question < ApplicationRecord
 
   # Kaminari
   paginates_per 5
+
+  scope :_search_, ->(page, term){
+      Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page) 
+  }
+
+  scope :last_questions, ->(page){
+    Question.includes(:answers).order('created_at desc').page(page)
+  }
 end
