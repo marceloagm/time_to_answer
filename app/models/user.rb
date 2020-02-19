c38093378c0779f8fcdb228b4b84da56657d49b3
marceloagm <marceloagmagalhaes@gmail.com>
2020-02-19 20:12:19 +0000

    Atualizando os dados do usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+
+  def full_name
+    [self.first_name, self.last_name].join(' ') #junta os nomes e coloca um espaço no meio com o join
+  end
 end
