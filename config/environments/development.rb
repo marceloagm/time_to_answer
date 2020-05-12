f01ed4cd96d1a6306a86d38d3cc29975b88aee68
marceloagm <marceloagmagalhaes@gmail.com>
2020-05-12 17:46:16 +0000

    Recuperar Senha

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,18 @@ Rails.application.configure do
   # Devise Config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+
+  #GMAIL CONFIG
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'apostenocfc@gmail.com',
+    password:             '|PP|pp1992',
+    authentication:       'plain',
+    enable_starttls_auto: true }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
